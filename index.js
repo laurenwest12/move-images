ec848332d414420dae66ed02238ec748675fc259
laurenwest12 <laurenwest12@gmail.com>
2021-04-01 15:27:54 -0400

    Added log files

--- index.js
+++ index.js
@@ -3,16 +3,21 @@ const app = express();
 const fs = require('fs');
 const sql = require('msnodesqlv8');
 
-const { destinationFolder } = require('./config');
-const { server, database, driver, query } = require('./config');
+const {
+	destinationFolder,
+	server,
+	database,
+	driver,
+	query,
+} = require('./config');
 
 const connectionString = `server=${server};Database=${database};Trusted_Connection=Yes;Driver=${driver}`;
 
 const moveAllFiles = async () => {
-	await sql.query(connectionString, query, (err, rows) => {
-		let submitted = [];
-		let rewrites = [];
-		let errors = [];
+	await sql.query(connectionString, query, (err1, rows) => {
+		let submitted = [['FilePath', 'FileName']];
+		let rewrites = [['FilePath', 'FileName']];
+		let errors = [['FilePath', 'ErrorMessage']];
 
 		for (let row of rows) {
 			const { Brand, FilePath, FILENAME } = row;
@@ -23,28 +28,40 @@ const moveAllFiles = async () => {
 
 			const destinationPath = `${destinationFolder}/${brandTrim}/${FILENAME}`;
 
-			console.log(file);
+			console.log(FILENAME);
 			try {
 				if (!fs.existsSync(`${destinationFolder}/${brandTrim}/`)) {
 					fs.mkdirSync(`${destinationFolder}/${brandTrim}/`);
 				}
 
 				if (fs.existsSync(destinationPath)) {
+					rewrites.push([FilePath, FILENAME]);
 					continue;
 				} else {
-					fs.copyFileSync(FilePath, destinationPath);
+					try {
+						fs.copyFileSync(FilePath, destinationPath);
+						submitted.push([FilePath, FILENAME]);
+					} catch (err2) {
+						errors.push([err2.path, err2.message]);
+					}
 				}
-			} catch (err) {
-				console.log(err);
-				errors.push(err);
+			} catch (err3) {
+				errors.push(err3.message);
 			}
 		}
-		console.log(errors);
+		let submittedCsv = submitted.map((e) => e.join(',')).join('\n');
+		let rewritesCsv = rewrites.map((e) => e.join(',')).join('\n');
+		let errorsCsv = errors.map((e) => e.join(',')).join('\n');
+
+		fs.writeFileSync(`${destinationFolder}/submitted.csv`, submittedCsv);
+		fs.writeFileSync(`${destinationFolder}/rewrites.csv`, rewritesCsv);
+		fs.writeFileSync(`${destinationFolder}/errors.csv`, errorsCsv);
+
+		console.log('Done');
 	});
 };
 
 app.listen(5050, async () => {
 	console.log('Server is running...');
 	await moveAllFiles();
-	console.log('Done moving files');
 });
