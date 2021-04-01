449a1ba70d1de57e2d9612ced70449cb90969b3d
laurenwest12 <laurenwest12@gmail.com>
2021-04-01 15:02:04 -0400

    Reworked for error reporting

--- index.js
+++ index.js
@@ -4,37 +4,43 @@ const fs = require('fs');
 const sql = require('msnodesqlv8');
 
 const { destinationFolder } = require('./config');
-const { server, database, driver } = require('./config');
+const { server, database, driver, query } = require('./config');
 
 const connectionString = `server=${server};Database=${database};Trusted_Connection=Yes;Driver=${driver}`;
 
 const moveAllFiles = async () => {
-	await sql.query(
-		connectionString,
-		'SELECT * FROM LabelPackImagesRL',
-		(err, rows) => {
-			for (let row of rows) {
-				const { tempcode, FileName } = row;
-				const fileArr = FileName.split('\\');
-				const file = fileArr[fileArr.length - 1];
+	await sql.query(connectionString, query, (err, rows) => {
+		let submitted = [];
+		let rewrites = [];
+		let errors = [];
 
-				const destinationPath = `${destinationFolder}/RALPH LAUREN/${file}`;
-				console.log(file);
-				try {
-					if (!fs.existsSync(`${destinationFolder}/RALPH LAUREN`)) {
-						fs.mkdirSync(`${destinationFolder}/RALPH LAUREN`);
-					}
-					if (fs.existsSync(destinationPath)) {
-						continue;
-					} else {
-						fs.copyFileSync(FileName, destinationPath);
-					}
-				} catch (err) {
-					console.log(err);
+		for (let row of rows) {
+			const { Brand, FilePath, FILENAME } = row;
+			const brandTrim = Brand.trim();
+
+			// const fileArr = FileName.split('\\');
+			// const file = fileArr[fileArr.length - 1];
+
+			const destinationPath = `${destinationFolder}/${brandTrim}/${FILENAME}`;
+
+			console.log(file);
+			try {
+				if (!fs.existsSync(`${destinationFolder}/${brandTrim}/`)) {
+					fs.mkdirSync(`${destinationFolder}/${brandTrim}/`);
 				}
+
+				if (fs.existsSync(destinationPath)) {
+					continue;
+				} else {
+					fs.copyFileSync(FilePath, destinationPath);
+				}
+			} catch (err) {
+				console.log(err);
+				errors.push(err);
 			}
 		}
-	);
+		console.log(errors);
+	});
 };
 
 app.listen(5050, async () => {
